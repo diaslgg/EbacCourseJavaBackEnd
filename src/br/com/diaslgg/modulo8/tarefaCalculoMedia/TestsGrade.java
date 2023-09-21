d655a1716694e6d01bbc6cd517bb7f47a366881f
luisdias <dias.lgg@gmail.com>
2023-09-21 06:15:32 -0300

    Exercicio modulo 8

---
+++ src/br/com/diaslgg/modulo8/tarefaCalculoMedia/TestsGrade.java
@@ -0,0 +1,46 @@
+package br.com.diaslgg.modulo8.tarefaCalculoMedia;
+
+public class TestsGrade {
+
+    private double firstGrade;
+    private double secondGrade;
+    private double thirdGrade;
+    private double forthGrade;
+
+    public TestsGrade(double first, double second, double third, double forth){
+        this.firstGrade = first;
+        this.secondGrade = second;
+        this.thirdGrade = third;
+        this.forthGrade = forth;
+
+    }
+
+    public double getFirstGrade() {
+        return this.firstGrade;
+    }
+
+    public double getSecondGrade() {
+        return this.secondGrade;
+    }
+
+    public double getThirdGrade() {
+        return this.thirdGrade;
+    }
+
+    public double getForthGrade() {
+        return this.forthGrade;
+    }
+
+    public void CalculatedMedianGrade() {
+        double medianGrade;
+        medianGrade = (this.getFirstGrade() + this.getSecondGrade() + this.getThirdGrade() + this.getForthGrade()) / 4;
+
+        System.out.println("Your final grade was " + medianGrade);
+
+        if(medianGrade > 5) {
+            System.out.println("You have been approved. Congratulations!");
+        } else {
+            System.out.println("You haven't been approved. Sorry to hear that.");
+        }
+    }
+}
---
+++ src/br/com/diaslgg/modulo8/tarefaCalculoMedia/UsageExample.java
@@ -0,0 +1,9 @@
+package br.com.diaslgg.modulo8.tarefaCalculoMedia;
+
+public class UsageExample {
+
+    public static void main(String[] args) {
+        TestsGrade gradeTracker = new TestsGrade(7.52, 4.27, 6.50, 8.34 );
+        gradeTracker.CalculatedMedianGrade();
+    }
+}
